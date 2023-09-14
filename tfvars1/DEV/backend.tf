e589d5935e73fcb42fd4435ef74bd93dc14da377
lingaraj77 <lingarajumodhala123@gmail.com>
2023-09-14 13:12:05 -0400

    terraform2

---
+++ tfvars1/DEV/backend.tf
@@ -0,0 +1,4 @@
+bucket   = "roboshop-remote-state-1"
+    key = "tfvars"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking-1"
---
+++ tfvars1/PROD/backend.tf
@@ -0,0 +1,4 @@
+bucket   = "roboshop-remote-state-prod01"
+    key = "tfvars"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking-prod1"
